9493870ad9808d77334a763f3eb1c18b24c2a5ae
Ben10225 <110615463+Ben10225@users.noreply.github.com>
2022-08-20 00:27:06 +0800

    Update foodmap_page_north1.js

--- foodmap_page_north1.js
+++ foodmap_page_north1.js
@@ -498,35 +498,6 @@ function givetimeSelection_7_1(selValue) {
 givetimeSelection_7_1(workornot.value);
 
 
-
-
-//------------------------
-/*
-var input = document.getElementById('image_uploads');
-var preview = document.querySelector('.preview_img');
-
-input.style.opacity = 0;
-input.addEventListener('change', updateImageDisplay);function updateImageDisplay() {
-	while(preview.firstChild) {
-		preview.removeChild(preview.firstChild);
-	}
-
-	if(input.files.length === 0) {
-		var para = document.createElement('p');
-		para.textContent = '未選擇任何檔案';
-    para.style="line-height: 300px;";
-		preview.appendChild(para);
-	} 
-	else {
-		var para = document.createElement('p');
-		var image = document.createElement('img');
-		image.src = window.URL.createObjectURL(input.files[0]);
-		preview.appendChild(image);
-		preview.appendChild(para);
-	}
-}
-*/
-
 // -------------------------
 $(function() {
   var imagesPreview = function(input, img) {
